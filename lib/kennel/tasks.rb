2377cf77795ec0cd91fe11a047814f51eaeef8dd
Michael Grosser <michael@grosser.it>
2019-11-25 13:08:05 -0800

    start testing rake tasks + fix no-data ignores not working

--- lib/kennel/tasks.rb
+++ lib/kennel/tasks.rb
@@ -4,12 +4,23 @@ require "kennel"
 require "kennel/unmuted_alerts"
 require "kennel/importer"
 
+module Kennel
+  module Tasks
+    class << self
+      def abort(message = nil)
+        Kennel.err.puts message if message
+        raise SystemExit, message
+      end
+    end
+  end
+end
+
 namespace :kennel do
   desc "Ensure there are no uncommited changes that would be hidden from PR reviewers"
   task no_diff: :generate do
     result = `git status --porcelain`.strip
-    abort "Diff found:\n#{result}\nrun `rake generate` and commit the diff to fix" unless result == ""
-    abort "Error during diffing" unless $CHILD_STATUS.success?
+    Kennel::Tasks.abort "Diff found:\n#{result}\nrun `rake generate` and commit the diff to fix" unless result == ""
+    Kennel::Tasks.abort "Error during diffing" unless $CHILD_STATUS.success?
   end
 
   # ideally do this on every run, but it's slow (~1.5s) and brittle (might not find all + might find false-positives)
@@ -38,7 +49,7 @@ namespace :kennel do
       url = (subdomain ? "https://zendesk.datadoghq.com" : "") + "/account/settings"
       puts "Invalid mentions found, either ignore them by adding to `KNOWN` env var or add them via #{url}"
       bad.each { |f, v| puts "Invalid mention #{v} in monitor message of #{f}" }
-      abort
+      Kennel::Tasks.abort
     end
   end
 
@@ -76,16 +87,16 @@ namespace :kennel do
 
   desc "show unmuted alerts filtered by TAG, for example TAG=team:foo"
   task alerts: :environment do
-    tag = ENV["TAG"] || abort("Call with TAG=foo:bar")
+    tag = ENV["TAG"] || Kennel::Tasks.abort("Call with TAG=foo:bar")
     Kennel::UnmutedAlerts.print(Kennel.send(:api), tag)
   end
 
   desc "show monitors with no data by TAG, for example TAG=team:foo"
   task nodata: :environment do
-    tag = ENV["TAG"] || abort("Call with TAG=foo:bar")
+    tag = ENV["TAG"] || Kennel::Tasks.abort("Call with TAG=foo:bar")
     monitors = Kennel.send(:api).list("monitor", monitor_tags: tag, group_states: "no data")
     monitors.select! { |m| m[:overall_state] == "No Data" }
-    monitors.reject! { |m| m[:tags].include? ["nodata:ignore"] }
+    monitors.reject! { |m| m[:tags].include? "nodata:ignore" }
     if monitors.any?
       Kennel.err.puts <<~TEXT
         This is a useful task to find monitors that have mis-spelled metrics or never received data at any time.
@@ -103,8 +114,8 @@ namespace :kennel do
 
   desc "Convert existing resources to copy-pastable definitions to import existing resources RESOURCE=dash ID=1234"
   task import: :environment do
-    resource = ENV["RESOURCE"] || abort("Call with RESOURCE=dash") # TODO: add others
-    id = ENV["ID"] || abort("Call with ID=1234")
+    resource = ENV["RESOURCE"] || Kennel::Tasks.abort("Call with RESOURCE=dash") # TODO: add others
+    id = ENV["ID"] || Kennel::Tasks.abort("Call with ID=1234")
     id = Integer(id) if id =~ /^\d+$/ # dashboards can have alphanumeric ids
     puts Kennel::Importer.new(Kennel.send(:api)).import(resource, id)
   end
--- test/kennel/tasks_test.rb
+++ test/kennel/tasks_test.rb
@@ -1,7 +1,55 @@
 # frozen_string_literal: true
 require_relative "../test_helper"
+require "rake"
+require "kennel/tasks"
 
-SingleCov.not_covered! # idk how to test this
+SingleCov.covered! uncovered: 39 # TODO: reduce this
 
 describe "tasks" do
+  with_env DATADOG_APP_KEY: "foo", DATADOG_API_KEY: "bar"
+
+  def execute(env = {})
+    with_env(env) { Rake::Task[task].execute }
+  rescue SystemExit
+    raise "Aborted #{$!.message}"
+  end
+
+  capture_all
+
+  describe "kennel:nodata" do
+    before { Kennel.send(:api).stubs(:list).returns monitors }
+
+    let(:task) { "kennel:nodata" }
+    let(:monitors) do
+      [{
+        name: "Foo",
+        id: 123,
+        overall_state: "No Data",
+        tags: []
+      }]
+    end
+
+    it "reports missing data" do
+      execute TAG: "team:foo"
+      stdout.string.must_equal "Foo\n/monitors/123\n\n"
+      stderr.string.must_include "nodata:ignore"
+    end
+
+    it "stops without TAG" do
+      e = assert_raises(RuntimeError) { execute }
+      e.message.must_equal "Aborted Call with TAG=foo:bar"
+    end
+
+    it "ignores monitors with data" do
+      monitors[0][:overall_state] = "OK"
+      execute TAG: "team:foo"
+      stdout.string.must_equal ""
+    end
+
+    it "ignores monitors marked as having no-data" do
+      monitors[0][:tags] = ["nodata:ignore"]
+      execute TAG: "team:foo"
+      stdout.string.must_equal ""
+    end
+  end
 end
