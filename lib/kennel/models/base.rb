7a859d2a6ff0a3f2cafcf25f81360396800aa1d5
Michael Grosser <michael@grosser.it>
2022-04-08 17:37:01 -0700

    allow overwriting kennel_id

--- lib/kennel/models/base.rb
+++ lib/kennel/models/base.rb
@@ -10,11 +10,7 @@ module Kennel
       SETTING_OVERRIDABLE_METHODS = [:name, :kennel_id].freeze
 
       def kennel_id
-        name = self.class.name
-        if name.start_with?("Kennel::") # core objects would always generate the same id
-          raise_with_location ArgumentError, "Set :kennel_id"
-        end
-        @kennel_id ||= Utils.snake_case name
+        @kennel_id ||= Utils.snake_case kennel_id_base
       end
 
       def name
@@ -23,6 +19,17 @@ module Kennel
 
       def to_json # rubocop:disable Lint/ToJSON
         raise NotImplementedError, "Use as_json"
+      end
+
+      private
+
+      # hook to allow overwriting id generation to remove custom module scopes
+      def kennel_id_base
+        name = self.class.name
+        if name.start_with?("Kennel::") # core objects would always generate the same id
+          raise_with_location ArgumentError, "Set :kennel_id"
+        end
+        name
       end
     end
   end
