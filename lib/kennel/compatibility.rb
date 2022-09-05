bffbd7e6ca5c234ce973052e73c3b0d3f2f1b100
Rachel Evans <revans@zendesk.com>
2022-09-05 10:08:40 +0200

    Expose build_default separately

--- lib/kennel/compatibility.rb
+++ lib/kennel/compatibility.rb
@@ -3,17 +3,21 @@ module Kennel
     def self.included(into)
       class << into
         %I[out out= err err= strict_imports strict_imports= generate plan update].each do |sym|
-          define_method(sym) { |*args| default_instance.public_send(sym, *args) }
+          define_method(sym) { |*args| instance.public_send(sym, *args) }
+        end
+
+        def build_default
+          Kennel::Engine.new
+        end
+
+        def instance
+          @default_instance ||= build_default
         end
 
         private
 
-        def default_instance
-          @default_instance ||= Kennel::Engine.new
-        end
-
         def api
-          default_instance.send(:api)
+          instance.send(:api)
         end
       end
     end
