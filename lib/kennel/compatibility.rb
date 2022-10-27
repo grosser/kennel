718b4677069d98ac3ae45b4e05a3df0e7205d6c4
Rachel Evans <revans@zendesk.com>
2022-10-27 06:25:59 +0200

    Remove Kennel.strict_imports

--- lib/kennel/compatibility.rb
+++ lib/kennel/compatibility.rb
@@ -4,7 +4,7 @@ module Kennel
   module Compatibility
     def self.included(into)
       class << into
-        %I[out out= err err= strict_imports strict_imports=].each do |sym|
+        %I[out out= err err=].each do |sym|
           define_method(sym) { |*args| instance.public_send(sym, *args) }
         end
 
