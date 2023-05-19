fd57182919584c65bb35176fdbda4dd2254bff82
Michael Grosser <michael@grosser.it>
2023-05-19 13:52:17 -0700

    Revert "ignore tags until the api is stable"
    This reverts commit f6486844a3b127c2e2ebc8f92ae55b58cb7850cf.

--- lib/kennel/models/dashboard.rb
+++ lib/kennel/models/dashboard.rb
@@ -5,8 +5,7 @@ module Kennel
       include TemplateVariables
 
       READONLY_ATTRIBUTES = superclass::READONLY_ATTRIBUTES + [
-        :author_handle, :author_name, :modified_at, :deleted_at, :url, :is_read_only, :notify_list, :restricted_roles,
-        :tags
+        :author_handle, :author_name, :modified_at, :deleted_at, :url, :is_read_only, :notify_list, :restricted_roles
       ]
       TRACKING_FIELD = :description
       REQUEST_DEFAULTS = {
