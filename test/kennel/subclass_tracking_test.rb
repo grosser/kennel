5e469fcd53423c630868d59d6925fa9c1985917f
Michael Grosser <michael@grosser.it>
2026-01-17 18:54:52 -0800

    allow defining abstract classes so project parts do not need to be defined

--- test/kennel/subclass_tracking_test.rb
+++ test/kennel/subclass_tracking_test.rb
@@ -11,4 +11,23 @@ describe Kennel::SubclassTracking do
       Kennel::Models::Project.recursive_subclasses.must_equal [TestProject, SubTestProject]
     end
   end
+
+  describe ".abstract_class!" do
+    it "marks self but not subclasses" do
+      parent = Class.new(Kennel::Models::Project)
+      parent.send(:abstract_class!)
+      assert parent.abstract_class?
+
+      begin
+        child = Class.new(parent)
+        refute child.abstract_class?
+
+        refute Kennel::Models::Project.abstract_class?
+      ensure
+        Kennel::Models::Project.subclasses.delete(child)
+      end
+    ensure
+      Kennel::Models::Project.subclasses.delete(parent)
+    end
+  end
 end
